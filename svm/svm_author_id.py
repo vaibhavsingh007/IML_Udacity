d653acf1d203f4355a67d50b7b9e0b0fa9d17357
vaibhav singh <vaibhav.singh21@yahoo.co.in>
2018-01-07 09:08:55 -0600

    Completed Naive Bayes, SVM and DTL mini projects.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,10 +20,27 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+clf = svm.SVC(kernel='rbf', C=10000)
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print(sum(x == 1 for x in pred))
+
+acc = accuracy_score(labels_test, pred)
+print(acc)
 
 #########################################################
 
