dcfbd6552f7a6546684cfd2b48f16536d99c2ca1
vaibhav singh <vaibhav.singh21@yahoo.co.in>
2018-01-29 14:58:09 -0600

    Completed PCA and Validations.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,6 +12,10 @@
 
 import pickle
 import sys
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -25,7 +29,12 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+feature_train, feature_test, label_train, label_test = train_test_split(features, labels, test_size=.3, random_state=42)
 
+clf = tree.DecisionTreeClassifier()
+clf.fit(feature_train, label_train)
+
+print(accuracy_score(label_test, clf.predict(feature_test)))
 
 ### it's all yours from here forward!  
 
